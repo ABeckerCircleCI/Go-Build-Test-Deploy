9ac64c95878afeacbbc32506de24e95fdc4065d1
ABeckerCircleCI <91962637+ABeckerCircleCI@users.noreply.github.com>
2023-09-29 16:02:41 -0400

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -6,41 +6,41 @@ import (
 )
 
 func Test_1(t *testing.T) {
-	time.Sleep(1 * time.Second)
-}
-
-func Test_2(t *testing.T) {
-	time.Sleep(1 * time.Second)
-}
-
-func Test_3(t *testing.T) {
-	time.Sleep(1 * time.Second)
-}
-
-func Test_4(t *testing.T) {
-	time.Sleep(4 * time.Second)
-}
-
-func Test_5(t *testing.T) {
 	time.Sleep(5 * time.Second)
 }
 
+func Test_2(t *testing.T) {
+	time.Sleep(5 * time.Second)
+}
+
+func Test_3(t *testing.T) {
+	time.Sleep(5 * time.Second)
+}
+
+func Test_4(t *testing.T) {
+	time.Sleep(20 * time.Second)
+}
+
+func Test_5(t *testing.T) {
+	time.Sleep(25 * time.Second)
+}
+
 func Test_6(t *testing.T) {
-	time.Sleep(3 * time.Second)
+	time.Sleep(15 * time.Second)
 }
 
 func Test_7(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 }
 
 func Test_8(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 }
 
 func Test_9(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 }
 
 func Test_10(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 }
