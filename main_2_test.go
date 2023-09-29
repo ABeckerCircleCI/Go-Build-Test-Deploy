6ab728d3beb790da958468ff143e70ebdea4ee1d
ABeckerCircleCI <91962637+ABeckerCircleCI@users.noreply.github.com>
2023-09-29 16:10:19 -0400

    Create main_2_test.go

---
+++ main_2_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_4(t *testing.T) {
+	time.Sleep(20 * time.Second)
+}
+
+func Test_5(t *testing.T) {
+	time.Sleep(25 * time.Second)
+}
+
+func Test_6(t *testing.T) {
+	time.Sleep(15 * time.Second)
+}
